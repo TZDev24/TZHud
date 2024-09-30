ce1ad7b6906549ea0c45b7babbb77cea587d8b3d
Skybox2011 <skybox2011@protonmail.com>
2024-09-30 02:12:57 -0400

    Show player health and armor

--- tzhud.lua
+++ tzhud.lua
@@ -18,26 +18,41 @@ end)
 w = ScrW()
 h = ScrH()
 
-HealthWindowW = w / 6
-HealthWindowH = h / 6
+healthBoxW = w / 6
+healthBoxH = h / 16
 
-HPWindowHorizMargin = w - w + HealthWindowW / 8
-HPWindowVertMargin = h - (ScrH() / 5)
+healthBoxPosX = w / 2 - (healthBoxW / 2)
+healthBoxPosY = h - (ScrH() / 12)
 
-AmmoWindowW = HealthWindowW
-AmmoWindowH = HealthWindowH
-
-AmmoWindowHorizMargin = ScrW() / 2 + ScrW() / 32
-AmmoWindowVMargin = h - (ScrH() / 5)
-
-print(w)
-print(h)
 -- Now we draw our own hud
 hook.Add("HUDPaint", "TZHud_Draw", function()
 	surface.SetDrawColor(0, 0, 0, 128)
 
-	surface.DrawRect(50, 50, 128, 128)
-	surface.DrawRect(HPWindowHorizMargin, HPWindowVertMargin, HealthWindowW, HealthWindowH)
+	-- Health and Armor background panel
+	surface.DrawRect(healthBoxPosX, healthBoxPosY, healthBoxW, healthBoxH)
 
-	surface.DrawRect(AmmoWindowHorizMargin, AmmoWindowVMargin, AmmoWindowW, AmmoWindowH)
+	-- Health
+	surface.SetFont("HudNumbersGlow")
+	surface.SetTextPos(healthBoxPosX, healthBoxPosY)
+	surface.SetTextColor(255, 0, 0)
+	surface.DrawText(LocalPlayer():Health())
+
+	surface.SetFont("HudNumbers")
+	surface.SetTextPos(healthBoxPosX, healthBoxPosY)
+	surface.DrawText(LocalPlayer():Health())
+
+	--Armor
+	surface.SetFont("HudNumbersGlow")
+	surface.SetTextPos(healthBoxPosX, healthBoxPosY + 32)
+	surface.SetTextColor(0, 96, 255)
+	surface.DrawText(LocalPlayer():Armor())
+
+	surface.SetFont("HudNumbers")
+	surface.SetTextPos(healthBoxPosX, healthBoxPosY + 32)
+	surface.DrawText(LocalPlayer():Armor())
+
+	-- Ammo (magazine / clip)
+	surface.SetFont("HudNumbersGlow")
+	surface.SetTextPos(healthBoxPosX + (healthBoxW - healthBoxW / 8), healthBoxPosY)
+	surface.DrawText(LocalPlayer():GetActiveWeapon():Clip1())
 end)
